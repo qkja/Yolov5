a9881f71f3188c36c8dae8f7bd7e58db37d4a642
qkj <qkj1205@163.com>
2023-12-03 15:22:42 +0800

    提交

---
+++ Test05/test.cpp
@@ -0,0 +1,23 @@
+//#define INPUT_TITLE "input image"
+//#include <opencv2/highgui/highgui_c.h>
+//#include<iostream>
+//#include<opencv2\opencv.hpp>
+//
+//using namespace std;
+//using namespace cv;
+//
+//int main() {
+//	Mat src;
+//	src = imread("./WIN_20230904_11_13_52_Pro.jpg");
+//	if (!src.data)
+//	{
+//		cout << "ERROR : could not load image.";
+//		return -1;
+//	}
+//
+//	namedWindow(INPUT_TITLE, CV_WINDOW_AUTOSIZE);
+//
+//	imshow(INPUT_TITLE, src);
+//	waitKey(0);
+//	return 0;
+//}
