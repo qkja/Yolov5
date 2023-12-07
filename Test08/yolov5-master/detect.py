b9f05c88be43da81e0e3a8690ff960f9e69d8811
qkj <qkj1205@163.com>
2023-12-07 11:12:12 +0800

    提交

--- Test08/yolov5-master/detect.py
+++ Test08/yolov5-master/detect.py
@@ -50,6 +50,7 @@ from utils.dataloaders import IMG_FORMATS, VID_FORMATS, LoadImages, LoadScreensh
 from utils.general import (LOGGER, Profile, check_file, check_img_size, check_imshow, check_requirements, colorstr, cv2,
                            increment_path, non_max_suppression, print_args, scale_boxes, strip_optimizer, xyxy2xywh)
 from utils.torch_utils import select_device, smart_inference_mode
+# python classify/predict.py --weights runs/train/exp/weights/best.pt --source data/images/WIN_20230904_14_58_56_Pro.jpg --device 0
 
 
 @smart_inference_mode()
